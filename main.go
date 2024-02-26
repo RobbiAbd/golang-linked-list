0f50d6e1dec423e2f04fe41d1e424d819f6534a5
robbiabd <robbyabd10@gmail.com>
2024-02-26 11:33:54 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type List struct {
+	head *Node
+}
+
+func main() {
+	list := &List{}
+	list.insertFirst(5)
+	list.insertFirst(10)
+	list.insertLast(15)
+	list.insertLast(20)
+
+	printList(list)
+	list.remove(15)
+	printList(list)
+}
+
+func (l *List) insertFirst(value int) {
+	newNode := &Node{data: value}
+
+	if l.head == nil {
+		l.head = newNode
+		return
+	}
+
+	newNode.next = l.head
+	l.head = newNode
+}
+
+func (l *List) insertLast(value int) {
+	newNode := &Node{data: value}
+
+	if l.head == nil {
+		l.head = newNode
+		return
+	}
+
+	curr := l.head
+	for curr.next != nil {
+		curr = curr.next
+	}
+
+	curr.next = newNode
+}
+
+func (l *List) remove(value int) {
+	if l.head == nil {
+		return
+	}
+
+	if l.head.data == value {
+		l.head = l.head.next
+		return
+	}
+
+	curr := l.head
+	for curr.next != nil && curr.next.data != value {
+		curr = curr.next
+	}
+
+	if curr.next != nil {
+		curr.next = curr.next.next
+	}
+}
+
+func printList(l *List) {
+	curr := l.head
+	for curr != nil {
+		fmt.Printf("%d ", curr.data)
+		curr = curr.next
+	}
+
+	fmt.Println()
+}
